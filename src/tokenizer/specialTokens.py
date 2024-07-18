569cabb698c5fada4803dbf5ee2f4d5743e34952
Ajay S Patil <51265917+ajayspatil7@users.noreply.github.com>
2024-07-18 23:53:23 +0530

    Update specialTokens.py

--- src/tokenizer/specialTokens.py
+++ src/tokenizer/specialTokens.py
@@ -1,9 +1,14 @@
 class ZephyraTokens:
+    # Padding token
     PAD = "<pad>"
+    # Beginning of sentence and End of sentence
     BOS = "<s>"
     EOS = "</s>"
+    # Junk
     UNK = "<unk>"
+    # Masking token
     MASK = "<mask>"
+    # Chat format tokens
     USER = "<|user|>"
     ASSISTANT = "<|assistant|>"
     SYSTEM = "<|system|>"

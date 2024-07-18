b1fd61def9813b4f99cfddd1229c34a9bcd7a58c
Ajay S Patil <51265917+ajayspatil7@users.noreply.github.com>
2024-07-18 23:45:56 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -9,13 +9,13 @@ from utils.trainArgs import train_epoch
 import config
 
 def main():
-    print("\nModules loaded correctly \n")
+    print("\nModules loaded correctly\n")
     
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(f"Device set to : [{device}] \n")
+    print(f"Device set to : [{device}]\n")
 
     tokenizer = ZephyraTokenizer()
-    print(f"Tokenizer loaded on [{device}] \n")
+    print(f"Tokenizer loaded on [{device}]\n")
 
     config.VOCAB_SIZE = tokenizer.get_vocab_size()
     
@@ -35,7 +35,7 @@ def main():
 
     optimizer = torch.optim.AdamW(model.parameters(), lr=config.LEARNING_RATE)
     
-    print(f"Optimiser loaded and training started on [{device}] \n")
+    print(f"Optimiser loaded and training started on [{device}]\n")
     for epoch in range(config.NUM_EPOCHS):
         train_loss = train_epoch(model, train_dataloader, optimizer, device)
         print(f"Epoch {epoch+1}/{config.NUM_EPOCHS}, Train Loss: {train_loss:.4f}")
